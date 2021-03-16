6610ae25d74fde15705316df88229cbb86c40d61
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-16 20:24:30 -0300

    feat: add subtraction demo function

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,3 +1,7 @@
 export function sum(num1: number, num2: number): number {
   return num1 + num2;
 }
+
+export function subtraction(num1: number, num2: number): number {
+  return num1 - num2;
+}
--- tests/calculate.spec.ts
+++ tests/calculate.spec.ts
@@ -1,8 +1,13 @@
-import { sum } from '@/calculate';
+import { subtraction, sum } from '@/calculate';
 
 describe('Calculate demo', () => {
   it('should return 2 when call sum with 1 and 1', () => {
     const result = sum(1, 1);
+    expect(result).toBe(2);
+  });
+
+  it('should return 2 when call subtraction with 5 and 3', () => {
+    const result = subtraction(5, 3);
     expect(result).toBe(2);
   });
 });
